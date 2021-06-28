67a45df830445b939ec0fa9c737f3eb5b9869ca0
Nathaniel Edwards <nedwa52@student.wgu.edu>
2021-06-28 15:17:37 -0600

    Fixed the damn View and Controler to read correct data

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sample.Model.InHouse;
 import sample.Model.Inventory;
-import sample.Model.Outsourced;
 import sample.Model.Product;
 
 public class Main extends Application {
@@ -22,16 +21,16 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
- /**       Outsourced test = new Outsourced(1,"Test", 22.00,4,1,5,"Outsourced test");
-        test.setCompanyName("Nate");
-        System.out.println(test.getCompanyName());
         Product testProduct = new Product(5, "Test Product", 5.99, 5, 1, 5);
         InHouse testerInHouse = new InHouse(5, "Test In House Product", 5.99, 5, 1, 5, 5);
-        System.out.println(testProduct.getName() +"\n"+testerInHouse.getName());
         Inventory.addProduct(testProduct);
-        System.out.println((Inventory.lookupProduct(5).getName()));
-  */
+        Inventory.addPart(testerInHouse);
+
+        launch(args);
+
+
+
+
 
 
     }
--- src/sample/Views/Controller.java
+++ src/sample/Views/Controller.java
@@ -1,4 +1,88 @@
 package sample.Views;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import sample.Model.InHouse;
+import sample.Model.Inventory;
+import sample.Model.Part;
+import sample.Model.Product;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static sample.Model.Inventory.*;
+import static sample.Model.Inventory.getAllParts;
+
+
+public class Controller implements Initializable {
+    @FXML
+    private TableView<Part> allPartsView;
+    @FXML
+    private TableColumn <Part, Integer> partIdCol;
+    @FXML
+    private TableColumn <Part,String>partNameCol;
+    @FXML
+    private TableColumn <Part, Integer>partInventory;
+    @FXML
+    private TableColumn <Part, Integer>partPrice;
+
+    @FXML
+    private TableView<Product> allProductsView;
+    @FXML
+    private TableColumn<Product, Integer> prodId;
+    @FXML
+    private TableColumn <Product, String> prodName;
+    @FXML
+    private TableColumn <Product, Integer> prodInventory;
+    @FXML
+    private TableColumn<Product, Integer>  prodPrice;
+
+    @FXML
+    private Button removePart;
+    @FXML
+    private Button removeProd;
+
+
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        allPartsView.setItems(getAllParts());
+        allProductsView.setItems(getAllProducts());
+
+        partIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        partNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        partInventory.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        partPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        prodId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        prodName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        prodInventory.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        prodPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+
+
+    }
+
+
+    public void onRemovePart(ActionEvent actionEvent) {
+        System.out.println("You Clicked Remove Part");
+    }
+
+    public void onRemoveProd(ActionEvent actionEvent) {
+        System.out.println("You Clicked Remove Product");
+    }
+
 }
