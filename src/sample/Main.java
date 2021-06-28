9f59db47b31d11f96a710525a29d7795d5ac087f
Nathaniel Edwards <nedwa52@student.wgu.edu>
2021-06-28 15:45:44 -0600

    Fixed the damn View and Controler to Remove objects from the list.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -21,11 +21,23 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        Product testProduct = new Product(5, "Test Product", 5.99, 5, 1, 5);
-        InHouse testerInHouse = new InHouse(5, "Test In House Product", 5.99, 5, 1, 5, 5);
-        Inventory.addProduct(testProduct);
-        Inventory.addPart(testerInHouse);
+        Product testProduct1 = new Product(1, "Test Product1", 5.99, 1, 1, 1);
+        InHouse testerInHouse1 = new InHouse(1, "Test In House Product1", 1, 1, 1, 1, 1);
+        Product testProduct2 = new Product(2, "Test Product2", 2, 2, 1, 5);
+        InHouse testerInHouse2 = new InHouse(2, "Test In House Product2", 5.99, 5, 1, 5, 5);
+        Product testProduct3 = new Product(3, "Test Product3", 5.99, 5, 1, 5);
+        InHouse testerInHouse3 = new InHouse(3, "Test In House Product3", 5.99, 5, 1, 5, 5);
+        Product testProduct4 = new Product(4, "Test Product4", 5.99, 5, 1, 5);
+        InHouse testerInHouse4 = new InHouse(4, "Test In House Product4", 5.99, 5, 1, 5, 5);
 
+        Inventory.addProduct(testProduct1);
+        Inventory.addPart(testerInHouse1);
+        Inventory.addProduct(testProduct2);
+        Inventory.addPart(testerInHouse2);
+        Inventory.addProduct(testProduct3);
+        Inventory.addPart(testerInHouse3);
+        Inventory.addProduct(testProduct4);
+        Inventory.addPart(testerInHouse4);
 
         launch(args);
 
