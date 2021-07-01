9397cee39701de19a63b4645fd176aa2a2fa69d0
Nathan Edwards <Natex338@gmail.com>
2021-07-01 17:14:54 -0600

    add part

--- src/sample/controller/AddRemovePart.java
+++ src/sample/controller/AddRemovePart.java
@@ -7,6 +7,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +18,16 @@ import java.util.ResourceBundle;
 
 public class AddRemovePart implements Initializable {
 
+
+    public TextField partID;
+    public TextField partName;
+    public TextField partInv;
+    public TextField partPrice;
+    public TextField partMax;
+    public TextField machineIDCompName;
+    public Label machineCompany;
+    public RadioButton inHousePart;
+    public RadioButton partOutsourced;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
